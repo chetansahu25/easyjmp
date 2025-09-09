82a20c79b1d9858e8f31f46d25b4f1b9ebba1d92
Chetan <chetankumarsahu2525@gmail.com>
2025-09-09 22:12:58 +0530

    changes cors origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://easyjmp-xdgv.vercel.app/",
     credentials: true,
 }))
 
